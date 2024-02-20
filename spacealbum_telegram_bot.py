3bb10baf080d97407831cea9ff345ae11742d930
juneshone <shamrova.julia@mail.ru>
2024-02-20 20:59:09 +0300

    publish photo to Telegram channel

--- spacealbum_telegram_bot.py
+++ spacealbum_telegram_bot.py
@@ -3,6 +3,13 @@ import telegram
 from dotenv import load_dotenv
 
 
-load_dotenv()
-bot = telegram.Bot(token=os.getenv('TELEGRAM_BOT_TOKEN'))
-bot.send_message(text="Hi! I'm a bot", chat_id=os.getenv('CHAT_ID'))
+def uploading_photo(telegram_bot_token, chat_id):
+    bot = telegram.Bot(token=telegram_bot_token)
+    bot.send_document(chat_id=chat_id, document=open('C:/Users/user/Desktop/API-lesson4/nasa_epic/epic_1.png', 'rb'))
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    telegram_bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
+    chat_id = os.getenv('CHAT_ID')
+    uploading_photo(telegram_bot_token, chat_id)
