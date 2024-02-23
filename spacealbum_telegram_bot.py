68aadb2e0e7499be9ebb90fec10275622a92808d
juneshone <shamrova.julia@mail.ru>
2024-02-23 18:04:30 +0300

    Использовала with open для открытия файлов

--- spacealbum_telegram_bot.py
+++ spacealbum_telegram_bot.py
@@ -6,29 +6,27 @@ import time
 from dotenv import load_dotenv
 
 
-def upload_selected_photo(bot_token, chat_id, filename):
-    bot = telegram.Bot(token=bot_token)
-    document = open(f'images/{filename}', 'rb')
-    bot.send_document(chat_id=chat_id, document=document)
+def upload_selected_photo(chat_id, filename):
+    with open(f'images/{filename}', 'rb') as document:
+        bot.send_document(chat_id=chat_id, document=document)
 
 
-def upload_random_photo(bot_token, chat_id, interval):
+def upload_random_photo(chat_id, interval):
     while True:
         for root, dirs, file in os.walk('images/'):
             filename = random.choice(file)
-            bot = telegram.Bot(token=bot_token)
-            document = open(f'images/{filename}', 'rb')
-            bot.send_document(chat_id=chat_id, document=document)
-            time.sleep(interval)
+            with open(f'images/{filename}', 'rb') as document:
+                bot.send_document(chat_id=chat_id, document=document)
+                time.sleep(interval)
 
 
 if __name__ == '__main__':
     load_dotenv()
-    bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
+    bot = telegram.Bot(token=os.getenv('TELEGRAM_BOT_TOKEN'))
     parser = argparse.ArgumentParser(
         description='Публикация фотографий в Telegram чате'
     )
-    parser.add_argument("--chat_id", help='Cсылка на чат, например, @chatChat')
+    parser.add_argument("--chat_id", required=True, help='Cсылка на чат, например, @chatChat')
     parser.add_argument("--interval",
                         type=int,
                         default=14400,
@@ -36,9 +34,9 @@ if __name__ == '__main__':
     parser.add_argument("--filename", help='Выбор определенной фотографии')
     args = parser.parse_args()
     if args.filename:
-        upload_selected_photo(bot_token, args.chat_id, args.filename)
+        upload_selected_photo(args.chat_id, args.filename)
     else:
         interval = int(os.getenv('PUBLICATION_INTERVAL'))
         if not interval:
             interval = args.interval
-        upload_random_photo(bot_token, args.chat_id, interval)
+        upload_random_photo(args.chat_id, interval)
